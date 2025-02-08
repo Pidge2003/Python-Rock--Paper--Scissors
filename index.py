ea912b5b5433895b1668505081d0db662003a1d7
Luke Partridge <lukej.partridge@icloud.com>
2025-02-08 19:11:30 +0000

    first commit

---
+++ index.py
@@ -0,0 +1,24 @@
+from random import choice
+import time
+
+moves = ('Rock': '🪨', 'Paper': '📄', 'Scissors': '✂')
+player = input('Rock, Paper or Scissors?').lower()
+computer = choice(list(moves.keys()))
+
+print(f\nYou: {moves[player]})
+time.sleep(0.5)
+print(f"Computer: {moves}[computer]")
+
+#Game Logic
+
+if player == computer:
+    print('❤️Tie!')
+
+elif (player == 'Rock' and computer == 'Scissors')  or/
+     (player == 'Paper' and computer == 'Rock')  or/
+     (player == 'Scissors' and computer == 'Paper')  or/
+     (player == 'Rock' and computer == 'Scissors')  or/
+     (player == 'Rock' and computer == 'Scissors')  or/
+    print("You win!")
+else:
+    print("You Lose")
